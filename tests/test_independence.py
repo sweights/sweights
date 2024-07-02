cbae99175fa13d562563de8b5120a62186fd6583
Hans Dembinski <HDembinski@users.noreply.github.com>
2024-07-02 15:29:43 +0200

    Rehabilitate kendall's tau test (#19)
    - New section for factorization_test.ipynb, explaining how to use
    Kendall's tau test correctly. It can be used if the samples for each
    component are available. However, these samples can in general only be
    obtained from simulation.
    - Use better formula for the expected error of the kendall's tau from
    scipy docs.
    - Update docstring of `kendall_tau` to explain how to use this test
    correctly.
    - Added unit test of kendall_tau.

--- tests/test_independence.py
+++ tests/test_independence.py
@@ -1,5 +1,18 @@
 import numpy as np
-from sweights.independence import plot_indep_scatter
+from sweights.independence import plot_indep_scatter, kendall_tau
+from scipy.stats import chi2
+from numpy.testing import assert_allclose
+
+
+def test_kendall_tau():
+    rng = np.random.default_rng(1)
+    x = rng.uniform(size=1000)
+    y = rng.uniform(size=1000)
+    val, err, pvalue = kendall_tau(x, y)
+    assert pvalue > 0.01
+    z = val / err
+    pvalue2 = chi2(1).sf(z**2)
+    assert_allclose(pvalue, pvalue2, atol=1e-3)
 
 
 def test_plot_indep_scatter():
